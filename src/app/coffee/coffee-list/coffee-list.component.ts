1708992d6a60d0d600ab7830a8885fe3d94375dd
Andres Ramirez <ha.ramirezg1@uniandes.edu.co>
2024-11-02 10:58:41 -0500

    Agregada logica del servicio, definida propiedades de clase e implementada peticion GET para la carga de los cafes

--- src/app/coffee/coffee-list/coffee-list.component.ts
+++ src/app/coffee/coffee-list/coffee-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CoffeeService } from '../coffee.service';
+import { Coffee } from '../coffee';
 
 @Component({
   selector: 'app-coffee-list',
   templateUrl: './coffee-list.component.html',
-  styleUrls: ['./coffee-list.component.css']
+  styleUrls: ['./coffee-list.component.css'],
 })
 export class CoffeeListComponent implements OnInit {
+  coffees: Array<Coffee> = [];
+  constructor(private coffeeService: CoffeeService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  getCoffees() {
+    this.coffeeService.getAllCoffees().subscribe((coffees) => {
+      this.coffees = coffees;
+    });
   }
 
+  ngOnInit() {
+    this.getCoffees();
+  }
 }
--- src/app/coffee/coffee.ts
+++ src/app/coffee/coffee.ts
@@ -1,2 +1,13 @@
 export class Coffee {
+  id: number;
+  nombre: string;
+  tipo: string;
+  region: string;
+
+  constructor(id: number, nombre: string, tipo: string, region: string) {
+    this.id = id;
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.region = region;
+  }
 }
